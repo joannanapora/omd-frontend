30ed3b05a2a8f15d95ba14e3c6995c7a7e30f150
Joanna <askanapora@gmail.com>
2020-11-14 15:00:20 +0100

    refactor: delete unused code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+import React from 'react':
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Taking care of your plants is EVERYTHING!
-        </p>
-        <a
-          className="App-link"
-          href="https://www.crocus.co.uk/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Buy your first plant!
-        </a>
-      </header>
     </div>
   );
 }
